87c4f9f5db6e3614bd1b1b5109c421b175f4d4cb
Daniel Machado <erevna2.engenharia@outlook.com>
2024-07-08 23:14:03 -0300

    Setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <>
+      <div className="">
+        <p>Convide seus amigos e planeje sua próxima viagem!</p>
+        <p>
+          Ao planejar sua viagem pela plann.er você automaticamente concorda
+          com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade.</a>
+        </p>
+      </div>
+    </>
+  )
+}
