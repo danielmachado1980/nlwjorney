b7d18d8d6ba6dc469a214c58ad98f1e762d3faed
Daniel Machado <erevna2.engenharia@outlook.com>
2024-07-08 23:21:17 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ export function App() {
       <div className="">
         <p>Convide seus amigos e planeje sua próxima viagem!</p>
         <p>
-          Ao planejar sua viagem pela plann.er você automaticamente concorda
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
           com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade.</a>
         </p>
       </div>
